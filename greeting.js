6d793753cf8720a79f07fbf960c897ce1bf311a1
rab87barnes <rab87barnes@aol.com>
2020-08-01 23:18:18 -0400

    first commit

---
+++ greeting.js
@@ -0,0 +1,5 @@
+//Here we set the function that will be exported in another file.//
+const Hello = function () {
+    console.log("Hi Let's get you started.")
+}
+module.exports = Hello
---
+++ userInput.js
@@ -0,0 +1,39 @@
+//Requiring file system, inquirer and greeting.js file//
+var fs = require ("fs");
+var userQuestions = require("inquirer");
+const Hello = require("./greeting.js");
+//Commanding the greeting from greeting.js//
+Hello ()
+function userQuestions(inquirer){
+//Inquirer is prompted to field questions//
+inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password"
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm"
+    }
+  ])
+  //Code functions for responses to the exchange of information in the prompt//
+  .then(function(data) {
+    var password = data.name;   
+    
+    fs.writeFile ('./userPassword.txt',data,function(err) {
+      if (response.confirm === response.password) {
+      console.log("Success! > userPassword.txt");
+      }
+      else {
+      console.log("You forgot your password already?!");
+      }
+    }); 
+      })}  
+//End of Code//      
